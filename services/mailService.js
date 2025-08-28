cb04f5784d1758726464d588f479df36ece34c1e
fahadwafiqsna-coder <fahadwafiq.sna@gmail.com>
2025-08-28 23:41:36 +0600

    Update mailService.js

--- services/mailService.js
+++ services/mailService.js
@@ -2,12 +2,22 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "mail.privateemail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
+
+  // const transporter = nodemailer.createTransport({
+  //   service: "gmail",
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASS,
+  //   },
+  // });
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
